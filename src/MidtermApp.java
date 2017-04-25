95577c046d3d0f69536ae423dff3da355f0c10f3
mtedder <maurice_tedder@hotmail.com>
2017-04-25 13:07:13 -0400

    add new println

--- src/MidtermApp.java
+++ src/MidtermApp.java
@@ -3,6 +3,8 @@
  */
 public class MidtermApp {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Dr. Syntax in da house!!!!");
     }
 }
