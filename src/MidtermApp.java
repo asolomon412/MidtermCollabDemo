276a4ed4daf681b86f7b6b53a04da264af21ea9d
mtedder <maurice_tedder@hotmail.com>
2017-04-25 13:21:06 -0400

    added third line

--- src/MidtermApp.java
+++ src/MidtermApp.java
@@ -6,6 +6,9 @@ public class MidtermApp {
 
         System.out.println("Hello World");
         System.out.println("Dr. Syntax in da house!!!!");
+
         System.out.println("I am adding another line of code");
+
+        System.out.println("3. Partay over here!!!");
     }
 }
